fb71bde5b5931e74e1ca0e3e1c5213a0d6b78276
microviver <tech@microviver.com>
2025-07-06 16:21:08 +0100

    Update popup.js

--- addons/website_discount_popup/static/src/js/popup.js
+++ addons/website_discount_popup/static/src/js/popup.js
@@ -1,20 +1,41 @@
-{
-    "name": "Website Discount Popup",
-    "version": "1.0",
-    "summary": "Show popup asking email in exchange for 5% discount",
-    "author": "Wadiana",
-    "license": "LGPL-3",
-    "depends": ["website"],
-    "data": [
-        "views/discount_popup_template.xml",
-    ],
-    "assets": {
-        "web.assets_frontend": [
-            "website_discount_popup/static/src/js/popup.js",
-            "website_discount_popup/static/src/css/popup.css",
-        ],
-    },
-    "installable": True,
-    "application": False,
-}
+odoo.define('website_discount_popup.popup', function (require) {
+    'use strict';
 
+    const publicWidget = require('web.public.widget');
+
+    publicWidget.registry.DiscountPopup = publicWidget.Widget.extend({
+        selector: 'body',
+        start: function () {
+            const popup = document.getElementById('discountPopup');
+            const btn = document.getElementById('submitDiscountBtn');
+
+            if (popup && !localStorage.getItem('discountPopupShown')) {
+                setTimeout(() => {
+                    popup.style.display = 'block';
+                    localStorage.setItem('discountPopupShown', 'true');
+                }, 1000);
+            }
+
+            if (btn) {
+                btn.addEventListener('click', function () {
+                    const email = document.getElementById("discountEmail").value;
+                    if (!email) return alert("Digite um email válido!");
+                    fetch("/discount/email", {
+                        method: "POST",
+                        headers: { 
+                            "Content-Type": "application/json",
+                            "X-CSRFToken": odoo.csrf_token
+                        },
+                        body: JSON.stringify({ email: email }),
+                    }).then(() => {
+                        popup.innerHTML = "<p>Obrigado! Desconto aplicado.</p>";
+                    });
+                });
+            }
+
+            return this._super(...arguments);
+        },
+    });
+
+    return publicWidget.registry.DiscountPopup;
+});
