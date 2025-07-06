637088330a81166014715a8022ed36ae1ecb256f
microviver <tech@microviver.com>
2025-07-06 13:42:02 +0100

    Update __manifest__.py

--- addons/custom_checkout_fields/__manifest__.py
+++ addons/custom_checkout_fields/__manifest__.py
@@ -3,9 +3,9 @@
     "version": "1.0",
     "author": "Wadiana",
     "depends": ["website_sale"],
-    'data': [
-    'views/website_sale_checkout.xml',
-    'views/res_partner_views.xml',
+    "data": [
+        "views/website_sale_checkout.xml",
+        "views/res_partner_views.xml"
     ],
     "installable": True,
     "application": False
