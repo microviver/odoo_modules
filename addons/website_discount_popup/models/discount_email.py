9d93e6b86a9937baad4414a5b5571ebbbdc23f50
espirulina <marcocarola@gmail.com>
2025-07-06 17:01:54 +0100

    fix: move website_discount_popup back into addons/

--- addons/website_discount_popup/models/discount_email.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class DiscountEmail(models.Model):
-    _name = 'discount.email.coupon'
-    _description = 'Email com desconto de popup'
-
-    email = fields.Char(required=True)
-    has_discount = fields.Boolean(default=True)
