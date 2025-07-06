02237cf1f6a635c3e47b66e417d2d583697ef1f1
microviver <tech@microviver.com>
2025-07-06 15:46:11 +0100

    Update __manifest__.py

--- addons/website_discount_popup/__manifest__.py
+++ addons/website_discount_popup/__manifest__.py
@@ -1,12 +1,19 @@
 {
     "name": "Website Discount Popup",
     "version": "1.0",
+    "summary": "Popup for email capture and discount offer",
     "author": "Wadiana",
-    "category": "Website",
+    "license": "LGPL-3",
     "depends": ["website"],
     "data": [
-        "views/discount_popup_template.xml"
+        "views/discount_popup_template.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_discount_popup/static/src/js/popup.js",
+            "website_discount_popup/static/src/css/popup.css",
+        ],
+    },
     "installable": True,
-    "application": False
+    "application": False,
 }
