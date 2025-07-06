cfa3ad1a750d5745cf98b77db90941fa0775b314
espirulina <marcocarola@gmail.com>
2025-07-06 07:32:09 +0100

    Adiciona módulo custom_checkout_fields para checkout personalizado

---
+++ addons/custom_checkout_fields/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Custom Checkout Fields",
+    "version": "1.0",
+    "author": "Wadiana",
+    "depends": ["website_sale"],
+    "data": [
+        "views/website_sale_checkout.xml"
+    ],
+    "installable": true,
+    "application": false
+}
