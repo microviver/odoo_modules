c57c0816b62a729a88767a72f137fe5e050dfb37
espirulina <marcocarola@gmail.com>
2025-07-06 17:14:33 +0100

    fix: move website_discount_popup to correct path

---
+++ addons/website_discount_popup/static/src/js/popup.js
@@ -0,0 +1,41 @@
+odoo.define('website_discount_popup.popup', function (require) {
+    'use strict';
+
+    const publicWidget = require('web.public.widget');
+
+    publicWidget.registry.DiscountPopup = publicWidget.Widget.extend({
+        selector: 'body',
+        start: function () {
+            const popup = document.getElementById('discountPopup');
+            const btn = document.getElementById('submitDiscountBtn');
+
+            if (popup && !localStorage.getItem('discountPopupShown')) {
+                setTimeout(() => {
+                    popup.style.display = 'block';
+                    localStorage.setItem('discountPopupShown', 'true');
+                }, 1000);
+            }
+
+            if (btn) {
+                btn.addEventListener('click', function () {
+                    const email = document.getElementById("discountEmail").value;
+                    if (!email) return alert("Digite um email válido!");
+                    fetch("/discount/email", {
+                        method: "POST",
+                        headers: { 
+                            "Content-Type": "application/json",
+                            "X-CSRFToken": odoo.csrf_token
+                        },
+                        body: JSON.stringify({ email: email }),
+                    }).then(() => {
+                        popup.innerHTML = "<p>Obrigado! Desconto aplicado.</p>";
+                    });
+                });
+            }
+
+            return this._super(...arguments);
+        },
+    });
+
+    return publicWidget.registry.DiscountPopup;
+});
