4697a84acd2307dad2e134c4621d79ef3643dd10
microviver <tech@microviver.com>
2025-07-07 19:15:28 +0100

    Update __manifest__.py

--- my_hello_popup/__manifest__.py
+++ my_hello_popup/__manifest__.py
@@ -1,21 +1,15 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "My Hello Popup",
-    'summary': "Um módulo Odoo simples para exibir um popup 'Hello !' em JavaScript.",
-    'description': """
-        Este módulo demonstra como incluir JavaScript customizado no Odoo 18
-        para exibir um popup de alerta simples.
-    """,
-    'author': "Seu Nome/Empresa",
-    'website': "http://www.seusite.com",
-    'category': 'Technical',
-    'version': '18.0.1.0.0',
-    'depends': ['web'],
-    'data': [
-        'views/assets.xml',
-    ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "name": "my_hello_popup",
+    "version": "1.0",
+    "summary": "Show hello popup on backend",
+    "author": "Wadiana",
+    "license": "LGPL-3",
+    "depends": ["web"],  # só precisa do 'web' para backend
+    "assets": {
+        "web.assets_backend": [
+            "my_hello_popup/static/src/js/hello_popup.js",
+        ]
+    },
+    "installable": True,
+    "application": False
 }
