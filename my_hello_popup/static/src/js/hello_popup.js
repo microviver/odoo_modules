91b1f6c0b099ed77a846a788c25204807f0fc670
microviver <tech@microviver.com>
2025-07-08 08:26:07 +0100

    Update hello_popup.js

--- my_hello_popup/static/src/js/hello_popup.js
+++ my_hello_popup/static/src/js/hello_popup.js
@@ -1,6 +1,9 @@
 /** @odoo-module **/
 
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("✅ hello_popup.js carregado com sucesso.");
-    alert("Hello!");
+odoo.define('my_hello_popup.hello_popup', function (require) {
+    "use strict";
+    document.addEventListener("DOMContentLoaded", function () {
+        console.log("✅ hello_popup.js carregado com sucesso.");
+        alert("Hello!");
+    });
 });
