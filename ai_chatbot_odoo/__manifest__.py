d66102d5bf3f901511972af3976f9a2527ac3d44
microviver <tech@microviver.com>
2025-07-08 13:45:24 +0100

    Update __manifest__.py

--- ai_chatbot_odoo/__manifest__.py
+++ ai_chatbot_odoo/__manifest__.py
@@ -6,17 +6,18 @@
     "license": "LGPL-3",
     "depends": ["base", "web", "website"],
     "assets": {
-    "web.assets_frontend": [
-        "web/static/lib/owl/owl.js",
-        "web/static/src/core/utils/functions.js",
-        "website/static/src/js/website.utils.js",
-        "ai_chatbot_odoo/static/src/js/chatbot.js",
-        "ai_chatbot_odoo/static/src/css/chatbot.css"
+        "web.assets_frontend": [
+            "web/static/lib/owl/owl.js",
+            "web/static/src/core/utils/functions.js",
+            "website/static/src/js/website.utils.js",
+            "ai_chatbot_odoo/static/src/js/chatbot.js",
+            "ai_chatbot_odoo/static/src/css/chatbot.css"
         ]
     },
-    "views/chatbot_template.xml",
+    "data": [
+        "views/chatbot_template.xml"
+    ],
     "installable": True,
     "application": False,
-    "test_disable": True,
+    "test_disable": True
 }
-
