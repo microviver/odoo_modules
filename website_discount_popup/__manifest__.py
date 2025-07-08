710484e513ff806f8d90aebdc5fe63dccd0d36b5
microviver <tech@microviver.com>
2025-07-08 09:07:07 +0100

    web assets frontent Update __manifest__.py

--- website_discount_popup/__manifest__.py
+++ website_discount_popup/__manifest__.py
@@ -7,6 +7,11 @@
     'depends': ['base', 'web', 'website'],
     "assets": {
         "web.assets_frontend": [
+            "web/static/src/legacy/js/public/public_widget.js",
+            "web/static/lib/owl/owl.js",  # OWL core, necessário para componentes OWL
+            "web/static/src/core/utils/functions.js",
+            "website/static/src/js/website.utils.js",
+            # Os teus ficheiros customizados:
             "website_discount_popup/static/src/js/popup.js",
             "website_discount_popup/static/src/css/popup.css",
         ],
