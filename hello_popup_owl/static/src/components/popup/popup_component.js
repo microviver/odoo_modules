e5a6ac2c53880e635c25df063333c233aff7fb58
espirulina <marcocarola@gmail.com>
2025-07-11 10:57:39 +0100

    friday 5

--- hello_popup_owl/static/src/components/popup/popup_component.js
+++ hello_popup_owl/static/src/components/popup/popup_component.js
@@ -1,18 +1,20 @@
 /** @odoo-module **/
+
 import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 
 export class PopupComponent extends Component {
   static template = "hello_popup_owl.PopupComponent";
+
   setup() {
     this.state = useState({ show: true });
   }
+
   close() {
     this.state.show = false;
   }
 }
 
-registry.category("public_components").add(
-  "hello_popup_owl.PopupComponent",
-  PopupComponent
-);
+registry.category("public_components").add("hello_popup_owl.PopupComponent", {
+  Component: PopupComponent,
+});
