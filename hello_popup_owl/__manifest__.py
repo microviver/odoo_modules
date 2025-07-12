d48dc242325862719f3658c4616d710317edab2c
espirulina <marcocarola@gmail.com>
2025-07-12 18:09:48 +0100

    gemini approach 6

--- hello_popup_owl/__manifest__.py
+++ hello_popup_owl/__manifest__.py
@@ -12,15 +12,5 @@
             "hello_popup_owl/static/src/js/popup_main.js", # <--- ADD THIS LINE
         ],
     },
-    "data": [
-        # popup_trigger_template.xml is only needed if it defines an Odoo view record,
-        # e.g., for creating a page. If its only purpose was to inject the script,
-        # you might not need it anymore. If it's a website template for some other
-        # purpose (e.g., a dynamic block), keep it.
-        # If its sole purpose was the <script> tag, you can probably remove it
-        # from 'data' and delete the file.
-        # Let's assume for now it defines a view, so keep it for now but remove the script inside.
-        "static/views/popup_trigger_template.xml",
-    ],
     "license": "LGPL-3",
 }
