6b8dc91541f72f03eb46cb5d21938fbcc506c621
espirulina <marcocarola@gmail.com>
2025-07-12 13:09:31 +0100

    saturday push

--- hello_popup_owl/__manifest__.py
+++ hello_popup_owl/__manifest__.py
@@ -3,14 +3,17 @@
     "version": "1.0",
     "category": "Website",
     "depends": ["website"],
-    "assets": {
-        "web.assets_frontend": [
-            "hello_popup_owl/static/src/components/popup/popup_template.xml",
-            "hello_popup_owl/static/src/main.js",
-            "hello_popup_owl/static/src/components/popup/popup_component.js"
-        ],
-    },
+    'assets': {
+    'web.assets_frontend': [
+        'hello_popup_owl/static/src/js/main.js',  # <-- CORRETO
+        'hello_popup_owl/static/src/components/popup/popup_component.js',
+        'hello_popup_owl/static/src/components/popup/popup_template.xml',
+        'hello_popup_owl/static/src/css/popup.css',
+    ],
+},
     "installable": True,
     "application": False,
     'license': 'LGPL-3'
 }
+
+
