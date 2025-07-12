066ee02a666c7960e5e6b11e261e596343d0c994
espirulina <marcocarola@gmail.com>
2025-07-12 16:30:58 +0100

    gemini

--- hello_popup_owl/__manifest__.py
+++ hello_popup_owl/__manifest__.py
@@ -3,14 +3,16 @@
     "version": "1.0",
     "summary": "Custom popup using OWL",
     "category": "Website",
-    "depends": ["website"],
+    "depends": ["web", "website"],
     "assets": {
         "web.assets_frontend": [
             "hello_popup_owl/static/src/js/main.js",
             "hello_popup_owl/static/src/components/popup/popup_component.js",
-            "hello_popup_owl/static/src/components/popup/popup_template.xml",
-            "hello_popup_owl/static/src/css/popup.css",
-        ],
+            "hello_popup_owl/static/src/css/popup.css"
+        ]
     },
+    "data": [
+        "static/src/components/popup/popup_template.xml"
+    ],
     "license": "LGPL-3",
 }
