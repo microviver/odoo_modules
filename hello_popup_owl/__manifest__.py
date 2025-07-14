47372eaf0351857bcd9bf421cffd99a39f94493c
espirulina <marcocarola@gmail.com>
2025-07-14 16:54:48 +0100

    monday

---
+++ hello_popup_owl/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Hello Popup OWL",
+    "version": "1.0",
+    "summary": "Custom popup using OWL",
+    "category": "Website",
+    "depends": ["website"],
+    "assets": {
+    "web.assets_frontend": [
+        "hello_popup_owl/static/src/js/main.js",
+        "hello_popup_owl/static/src/js/popup_trigger.js",
+        "hello_popup_owl/static/src/components/popup/popup_component.js",
+        "hello_popup_owl/static/src/components/popup/popup_template.xml",
+        "hello_popup_owl/static/src/css/popup.css"
+    ]
+},
+
+    "license": "LGPL-3",
+}
