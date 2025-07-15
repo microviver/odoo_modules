40e056c083abd7f6b6694e99326ec8be00f0cc6f
espirulina <marcocarola@gmail.com>
2025-07-15 09:51:48 +0100

    july version 3

--- ai_chatbot_odoo/__manifest__.py
+++ ai_chatbot_odoo/__manifest__.py
@@ -4,12 +4,14 @@
     'category': 'Tools',
     'summary': 'Chatbot integrado com OpenAI Assistant',
     'depends': ['base', 'web'],
-    'assets': {
+   'assets': {
     'web.assets_frontend': [
         'ai_chatbot_odoo/static/src/js/main.js',
+        'ai_chatbot_odoo/static/src/components/chatbot/chatbot_component.js',
         'ai_chatbot_odoo/static/src/components/chatbot/chatbot_template.xml',
         'ai_chatbot_odoo/static/src/styles/chatbot.scss',
     ],
-    },    
+},
+ 
     'installable': True,
 }
