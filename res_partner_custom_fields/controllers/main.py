dcd2cedc3b24e015b4fd5325e23f669962529832
espirulina <marcocarola@gmail.com>
2025-07-22 09:38:58 +0100

    wed 2

--- res_partner_custom_fields/controllers/main.py
+++ res_partner_custom_fields/controllers/main.py
@@ -1,16 +1,11 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
-from odoo import http
 
 class WebsiteSaleCustom(WebsiteSale):
 
     def checkout_form_save(self, mode, partner_values, all_values, checkout):
-        # Captura os campos personalizados enviados no /shop/address
-        if 'nombre' in all_values:
-            partner_values['nombre'] = all_values['nombre']
-        if 'apelido' in all_values:
-            partner_values['apelido'] = all_values['apelido']
-        if 'dni' in all_values:
-            partner_values['dni'] = all_values['dni']
+        partner_values['nombre'] = all_values.get('nombre', '')
+        partner_values['apelido'] = all_values.get('apelido', '')
+        partner_values['dni'] = all_values.get('dni', '')
         return super().checkout_form_save(mode, partner_values, all_values, checkout)
 
