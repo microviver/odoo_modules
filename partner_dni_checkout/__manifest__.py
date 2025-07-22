6c6dd64515fe87efc4737c92b7d45d30791d3694
espirulina <marcocarola@gmail.com>
2025-07-22 17:01:52 +0100

    new module custom fields

---
+++ partner_dni_checkout/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Partner DNI Checkout",
+    "version": "1.0",
+    "depends": ["website_sale", "contacts"],
+    "author": "Wadiana",
+    "category": "Website",
+    "summary": "Adiciona campo DNI no checkout da loja e no backend",
+    "data": [
+        "views/res_partner_views.xml",
+        "views/website_sale_templates.xml"
+    ],
+    "installable": True,
+    "application": False
+}
