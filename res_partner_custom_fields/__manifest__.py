571200be291625d82ae9f1bb0212eac4393a0467
espirulina <marcocarola@gmail.com>
2025-07-22 09:28:28 +0100

    wednesday 1

--- res_partner_custom_fields/__manifest__.py
+++ res_partner_custom_fields/__manifest__.py
@@ -5,8 +5,9 @@
     'summary': 'Divide campo Full Name e adiciona campo DNI obrigatório',
     'depends': ['base', 'contacts'],
     'data': [
-        'views/res_partner_views.xml',
-    ],
+    'views/res_partner_views.xml',
+    'views/website_sale_address_inherit.xml',
+	],
     'installable': True,
     'auto_install': False,
 }
