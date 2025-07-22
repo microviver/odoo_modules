d80839d1f3a3ab533e8f6f831e1bceda893aaa9a
espirulina <marcocarola@gmail.com>
2025-07-22 09:00:56 +0100

    new module for custom fields

---
+++ res_partner_custom_fields/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Dividir Nome e Adicionar DNI',
+    'version': '1.0',
+    'category': 'Contactos',
+    'summary': 'Divide campo Full Name e adiciona campo DNI obrigatório',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
