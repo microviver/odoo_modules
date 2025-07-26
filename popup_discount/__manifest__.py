a9d8fb3f5e7f8bcf2df80d5e49c80070ad4e4df0
espirulina <marcocarola@gmail.com>
2025-07-26 11:10:40 +0100

    new init inside models 2

--- popup_discount/__manifest__.py
+++ popup_discount/__manifest__.py
@@ -5,8 +5,6 @@
     "category": "Website",
     "depends": ["website_sale", "mass_mailing"],
       'data': [
-	    'models/discount_code.py',  # This line is critical
-	    'models/sale_order.py',
 	    'data/mail_template.xml',
 	    'views/*.xml',
 	],
