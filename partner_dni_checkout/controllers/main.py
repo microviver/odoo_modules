521c3ad129ec6e6135c857c6c9c29e09b8911780
espirulina <marcocarola@gmail.com>
2025-08-05 19:01:20 +0100

    gemini 1

--- partner_dni_checkout/controllers/main.py
+++ partner_dni_checkout/controllers/main.py
@@ -1,18 +1,11 @@
-from odoo import http
-from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
-
 
 class WebsiteSaleInherit(WebsiteSale):
 
-    def checkout_form_save(self, checkout, **kw):
-        """ Salva também first_name, last_name e dni no res.partner """
-        if kw.get('first_name'):
-            checkout['first_name'] = kw.get('first_name').strip()
-        if kw.get('last_name'):
-            checkout['last_name'] = kw.get('last_name').strip()
-        if kw.get('dni'):
-            checkout['dni'] = kw.get('dni').strip()
-
-        return super().checkout_form_save(checkout, **kw)
-
+    def checkout(self, **post):
+        # Override the checkout method to ensure 'partner' is in the qcontext
+        res = super(WebsiteSaleInherit, self).checkout(**post)
+        order = res.qcontext['website_sale_order']
+        partner = order.partner_id
+        res.qcontext['partner'] = partner
+        return res
